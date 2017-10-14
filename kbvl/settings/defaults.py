bea9a57510f0459bbc9f4e3b37d8220dba1359ac
Mesenev Pavel <mesenev@pashamac.local>
2017-10-14 20:30:56 +1000

    setup whitenoise for static

--- kbvl/settings/defaults.py
+++ kbvl/settings/defaults.py
@@ -18,6 +18,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'kbvl.urls'
@@ -68,6 +69,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
