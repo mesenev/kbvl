41b50bdc0aebb669fd9fe8fb2fb25506c51e21dc
Pavel Mesenev <heredemonsnearby@mail.ru>
2017-10-17 16:23:49 +1000

    add google search engine approve

--- kbvl/urls.py
+++ kbvl/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from landing_app.views import landing
 from django.http import HttpResponse
 
+ggl = "google-site-verification: google0e9c0b4f13a99fcf.html"
+
 urlpatterns = [
     url(r'^robots.txt$', lambda x: HttpResponse("User-Agent: *\nAllow: *", content_type="text/plain"), name="robots_file"),
+    url(r'^google0e9c0b4f13a99fcf.html$', lambda x: HttpResponse(ggl, content_type="text/html; charset=utf-8"), name="google_approve"),
     url(r'', landing),
 ]
