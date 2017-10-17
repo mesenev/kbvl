4035fbc79368fb8177e4e778fe0f5e321a6b93f0
Pavel Mesenev <heredemonsnearby@mail.ru>
2017-10-17 15:55:42 +1000

    Update urls.py
    add /robots.txt

--- kbvl/urls.py
+++ kbvl/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 from landing_app.views import landing
+from django.http import HttpResponse
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', landing)
+    url(r'', landing),
+    url(r'^robots.txt', lambda x: HttpResponse("User-Agent: *\nAllow: *", content_type="text/plain"), name="robots_file"),
 ]
