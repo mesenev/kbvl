5cc1ff96efc6de6ca7280454e7ffab6156b311fd
mesenev <mesenev.pr@gmail.com>
2017-10-17 16:08:08 +1000

    remove admin

--- kbvl/urls.py
+++ kbvl/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
-from django.contrib import admin
 from landing_app.views import landing
 from django.http import HttpResponse
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^robots.txt$', lambda x: HttpResponse("User-Agent: *\nAllow: *", content_type="text/plain"), name="robots_file"),
     url(r'', landing),
 ]
